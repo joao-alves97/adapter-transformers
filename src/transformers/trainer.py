4555a04e09f976bcc26558554f2ae733a04647c4
João Alves <joao.alves@unbabel.com>
2022-04-27 09:16:18 +0000

    small fix

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1503,7 +1503,7 @@ class Trainer:
                     self.optimizer = optimizer
                     self.lr_scheduler = lr_scheduler
                     self.deepspeed.load_checkpoint(
-                        self.state.best_model_checkpoint, load_optimizer_states=True, load_lr_scheduler_states=True
+                        self.state.best_model_checkpoint, load_optimizer_states=False, load_lr_scheduler_states=False
                     )
                 else:
                     # We load the model state dict on the CPU to avoid an OOM error.
